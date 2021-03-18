20d0cfd4dab324d41b4946a8e97d4187d4fb9a3e
Ahmet H Kaya <hudaiahmet@gmail.com>
2021-03-18 20:18:18 +0300

    JDK 8 JavaFX edit

--- src/main/java/com/ahk/ui/starter/Main.java
+++ src/main/java/com/ahk/ui/starter/Main.java
@@ -7,10 +7,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.Writer;
-
 public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
